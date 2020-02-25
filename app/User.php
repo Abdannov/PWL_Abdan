92f34ddecc0832ccd1cfa1d85ef0c1da44b619b4
abdannov <abdan.wa99@gmail.com>
2020-02-25 16:35:47 +0800

    tugas AuthenticationApi

--- app/User.php
+++ app/User.php
@@ -38,6 +38,31 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
 
+    public function artikels(){
+        return $this->hasMany(\App\Artikel::class, 'users_id', 'id');
+    }
+    public function kategoriArtikels(){
+        return $this->hasMany(\App\KategoriArtikel::class, 'users_id', 'id');
+    }
+    public function beritas(){
+        return $this->hasMany(\App\Berita::class, 'users_id', 'id');
+    }
+    public function kategoriBeritas(){
+        return $this->hasMany(\App\KategoriBerita::class, 'users_id', 'id');
+    }
+    public function galeris(){
+        return $this->hasMany(\App\Galeri::class, 'users_id', 'id');
+    }
+    public function kategoriGaleris(){
+        return $this->hasMany(\App\KategoriGaleri::class, 'users_id', 'id');
+    }
+    public function pengumumans(){
+        return $this->hasMany(\App\Pengumuman::class, 'users_id', 'id');
+    }
+    public function kategoriPengumumans(){
+        return $this->hasMany(\App\KategoriPengumuman::class, 'users_id', 'id');
+    }
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
